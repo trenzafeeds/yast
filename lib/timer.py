41bd436080741376f78f3df6194630250ceef47b
Kat Cannon-MacMartin <guthrie@marlboro.edu>
2021-08-19 13:25:29 -0400

    basic display, start/pause and splitting

---
+++ lib/timer.py
@@ -0,0 +1,77 @@
+"""
+timer.py
+"""
+
+import time, sys
+
+class Timer:
+    """
+    resume_data is a dict with:
+    resume_data = {
+    'label' = 'string'
+    'time' : x.x,
+    'splits' : [x.x, x.x, x.x, ... ]
+    }
+    """
+
+    def __init__(self, label = None, resume_data = None):
+        if resume_data:
+            self.label = resume_data['label']
+            self.current_time = resume_data['time']
+            self.splits = resume_data['splits']
+        else:
+            if label: self.label = label
+            else: self.label = "Unlabeled Timer"
+            self.current_time = 0.0
+            self.splits = [0.0]
+        self.started = False
+        self.active = False
+        self.init_time = None
+
+    def start(self):
+        if self.init_time != None:
+            print("Error: attempted to re-initialize timer.")
+            sys.exit(1)
+
+        self.init_time = time.time() - self.current_time
+        self.started = True
+        self.active = True
+        return self.current_time
+
+    def get_time(self):
+        if self.active: self.current_time = time.time() - self.init_time
+        return self.current_time
+
+    def pause(self):
+        p = self.get_time()
+        self.active = False
+        return p
+
+    def resume(self):
+        self.init_time = time.time() - self.current_time
+        self.active = True
+        return self.current_time
+
+    def split(self):
+        """ Returns new split as most recent split """
+        s = self.get_time()
+        self.splits.append(s)
+        return s
+
+    def unsplit(self):
+        """ Returns the split before deleted split as most recent split """
+        self.splits.pop()
+        return self.splits[-1]
+
+    def since_split(self):
+        return self.current_time - self.splits[-1]
+
+    def data(self):
+        d = {}
+        d['label'] = self.label
+        d['time'] = self.get_time()
+        d['splits'] = self.splits
+        return d
+    
+    
+
---
+++ lib/window.py
@@ -0,0 +1,102 @@
+"""
+window.py
+
+Functions for Qt window
+creation.
+
+Sources:
+[1] https://stackoverflow.com/questions/33982167/pyqt5-create-semi-transparent-window-with-non-transparent-children
+[2] https://stackoverflow.com/questions/49971584/updating-pyqt5-gui-with-live-data
+"""
+
+STARTPAUSEKEY = '<insert>'
+SPLITKEY = '<home>'
+
+from timer import *
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from pynput import keyboard
+
+def display_time(time_in_s):
+    return "{:0>2.0f}:{:0>2.0f}:{:0>2.2f}".format(time_in_s // 3600, time_in_s // 60, time_in_s % 60)
+
+# Following class from [2]
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setMinimumWidth(350)
+        self.setMinimumHeight(100)
+
+        # Styling
+        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WA_NoSystemBackground, True)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        
+        # Timer display
+        self.label = QLabel('0.0')
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setFont(QFont('Monospace', 20))
+        self.label.setStyleSheet("background-color: pink; color: white; border: 1px solid aqua;")
+        self.timer_name = QLabel('Untitled')
+        self.timer_name.setFont(QFont('Monospace', 12))
+        self.timer_name.setAlignment(Qt.AlignCenter)
+        self.timer_name.setStyleSheet("background-color: pink; color: white; border: 1px solid aqua;")
+        self.last_split = QLabel('No Split Yet')
+        self.last_split.setFont(QFont('Monospace', 10))
+        self.last_split.setAlignment(Qt.AlignCenter)
+        self.last_split.setStyleSheet("background-color: pink; color: white; border: 1px solid aqua;")
+
+        self.timer_w = QWidget()
+        self.w_layout = QGridLayout(self.timer_w)
+        self.w_layout.addWidget(self.timer_name, 0, 0)
+        self.w_layout.addWidget(self.last_split, 1, 1)
+        self.w_layout.addWidget(self.label, 0, 1)
+        self.setCentralWidget(self.timer_w)
+
+        self.qtimer = QTimer()
+        self.qtimer.setInterval(10)
+        self.qtimer.timeout.connect(self.updatetime)
+
+        self.hk_startpause = keyboard.GlobalHotKeys({'{}'.format(STARTPAUSEKEY):self.startpause})
+        self.hk_split = keyboard.GlobalHotKeys({'{}'.format(SPLITKEY):self.split})
+        self.hk_startpause.start()
+        self.hk_split.start()
+        
+        self.event_loop()
+
+    def event_loop(self):
+        self.rtimer = Timer()
+        self.updatesplit()
+        self.qtimer.start()
+
+    def updatetime(self):
+        ctime = self.rtimer.get_time()
+        self.label.setText(display_time(ctime))
+        self.updatesplit()
+
+    def updatesplit(self):
+        lsplit = self.rtimer.since_split()
+        self.last_split.setText("Split: {}".format(display_time(lsplit)))
+
+    def startpause(self):
+        if not self.rtimer.started: self.rtimer.start()
+        else:
+            if self.rtimer.active: self.rtimer.pause()
+            else: self.rtimer.resume()
+
+    def split(self):
+        self.rtimer.split()
+
+def main():
+    qapp = QApplication(sys.argv)
+    qapp.setStyle('Fusion')
+    qwin = MainWindow()
+    qwin.show()
+    sys.exit(qapp.exec_())
+    
+if __name__=='__main__':
+    main()
+            
